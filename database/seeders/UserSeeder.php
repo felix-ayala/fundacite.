385749f6eb243086aa326408682c1a3f39554823
Yohangel López <yohangel.lopez.yl@gmail.com>
2023-06-08 21:39:33 -0400

    rol

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,6 +18,12 @@ class UserSeeder extends Seeder
             'email'=> 'admin@example.com',
             'password'=> bcrypt('12345678')
         ])->assignRole('Administrador');
+        
+        User::create([
+            'name' => 'Transcriptor',
+            'email'=> 'transcriptor@example.com',
+            'password'=> bcrypt('12345678')
+        ])->assignRole('Transcriptor');
 
     }
 }
